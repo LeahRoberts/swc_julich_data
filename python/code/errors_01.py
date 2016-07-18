1c8e380c99789f5c94902290188a6901c33e5ac6
Leah Roberts <l.roberts3@uq.edu.au>
2016-07-18 23:03:49 +1000

    fixing scripts so they work

--- python/code/errors_01.py
+++ python/code/errors_01.py
@@ -1,7 +1,11 @@
-def favorite_ice_cream():
+df favorite_ice_cream():
     ice_creams = [
         "chocolate",
         "vanilla",
         "strawberry"
     ]
-    print ice_creams[3]
+    print(ice_creams[3])
+
+if __name__ == '__main__':
+
+   favorite_ice_cream()
